bd2bd3f10b6ea0849bf48c1001d68f257078cd10
Renaud Bernon <renaudbernon@hotmail.com>
2015-12-03 10:49:55 +0100

    added Loginscreen added login screen with ng-view from index.html added custom bootstrap from http://startbootstrap.com/template-overviews/sb-admin/

---
+++ Server/public/app/app.js
@@ -0,0 +1,28 @@
+(function () {
+    'use strict';
+    // 1. Module definieren
+    angular.module('myApp', ['ngRoute'])
+        .config(moduleConfig);
+
+    // 2. Inject dependencies
+    moduleConfig.$inject = ['$routeProvider'];
+
+    // 3. Routes configureren
+    function moduleConfig($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'views/login.html',
+            //controller: 'loginController',
+            //controllerAs: 'loginCtrl'
+        })
+            .when('/login', {
+                templateUrl: 'views/login.html',
+                //controller: 'loginController',
+                //controllerAs: 'loginCtrl'
+            })
+            .otherwise({
+				//temporary
+                redirectTo: '/'
+				//redirectTo: 'views/404.html'
+            });
+    }
+})();
